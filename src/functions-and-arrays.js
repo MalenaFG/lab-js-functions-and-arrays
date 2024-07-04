8efa83b77e5e8f43beff172d91fd9f189198d647
MalenaFG <malena.f.giusti@gmail.com>
2024-07-04 19:36:55 +0200

    All done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,13 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    } else if (num2 > num1) {
+        return num2
+    } else {
+        return num1
+    }
+}
 
 
 
@@ -7,15 +15,35 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arrOfWords) {
+    let longestString = ""
 
-
+    if (arrOfWords.length === 0) {
+        return null
+    } else {
+        for (let i = 0; i < arrOfWords.length; i++) {
+            let word = arrOfWords[i]
+            if (word.length > longestString.length) {
+                longestString = word
+            } else if (arrOfWords.length === 0) {
+                longestString = null
+            }
+        }
+        return longestString
+    }
+}
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arrOfNum) {
+    let sum = 0
+    for (let i = 0; i < arrOfNum.length; i++) {
+        sum += arrOfNum[i]
+    }
+    return sum
+}
 
 
 
@@ -23,7 +51,17 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arrOfNum) {
+    let sum = 0
+    if (arrOfNum.length === 0) {
+        return 0
+    } else {
+        for (let i = 0; i < arrOfNum.length; i++) {
+            sum += arrOfNum[i]
+        }
+        return sum / arrOfNum.length
+    }
+}
 
 
 
@@ -31,4 +69,12 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arrOfWords, word) {
+    if (arrOfWords.length === 0) {
+        return null
+    } else if (arrOfWords.includes(word)) {
+        return true
+    } else {
+        return false
+    }
+}
